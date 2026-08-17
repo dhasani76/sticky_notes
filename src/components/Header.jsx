6467d4b503a938b83b1959de89cf6a19e5f8c409
Dritan Hasani <dhasani76@outlook.com>
2026-08-17 14:56:20 -0400

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AddTaskIcon from "@mui/icons-material/AddTask";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <AddTaskIcon /> Sticky Notes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
